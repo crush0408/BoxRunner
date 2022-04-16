877928378c6ede7c95c8ff6e0a51a0735d23dd07
CRUSH04 <msng0408@naver.com>
2022-04-17 00:11:43 +0900

    맵 시스템 만들기 전 백업

---
+++ Assets/01.Scripts/Map/MapData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MapData
+{
+
+    public Vector3 startPos;
+    public Vector3 endPos;
+    public GameObject mapPrefab;
+}
